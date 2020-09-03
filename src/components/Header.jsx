1f941d931c5f4e022e3e908e7a4a6dbc23cc20bb
Daniel Martinez Pascual <daniellopascuallo@hotmail.com>
2020-09-02 21:45:34 -0400

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <>
+            <h1>Hello Dojo!</h1>
+            <h3>Things I need to do:</h3>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </>
+    );
+}
+
+export default Header;
+
+
